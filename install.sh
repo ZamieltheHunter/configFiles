f278710c17e3281249580e56163297c46fe4cbdd
Chris Barden <christopher.barden@magnetimarelli.com>
2017-12-20 18:05:52 -0500

    Fixed some directory issues in the install script and added sudo so oblogout.conf could be dropped in etc

--- install.sh
+++ install.sh
@@ -5,8 +5,12 @@ if [ -L $0 ] ; then
 else
     ME=$0
 fi
-DIR=$(dirname $ME)
 
+if [ $(dirname $ME) = "." ] ; then
+    DIR="$(pwd)"
+else
+    DIR="$(pwd)/$(dirname $ME)"
+fi
 mkdir -p $HOME/.oh-my-zsh/custom
 ln -is $DIR/custom/aliases.zsh $HOME/.oh-my-zsh/custom/
 ln -is $DIR/custom/exports.zsh $HOME/.oh-my-zsh/custom/
@@ -18,9 +22,9 @@ ln -is $DIR/openbox/rc.xml $HOME/.config/openbox/
 ln -is $DIR/openbox/autostart $HOME/.config/openbox/
 ln -is $DIR/backgrounds $HOME/.backgrounds
 
-ln -is vim $HOME/.vim
-ln -is vimrc $HOME/.vimrc
+ln -is $DIR/vim $HOME/.vim
+ln -is $DIR/vimrc $HOME/.vimrc
 
-ln -is conkyrc $HOME/.conkyrc
+ln -is $DIR/conkyrc $HOME/.conkyrc
 
-ln -is oblogout.conf /etc/oblogout.conf
+sudo ln -is $DIR/oblogout.conf /etc/oblogout.conf
