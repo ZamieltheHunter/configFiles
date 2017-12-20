365dbad6d80dbc902cbcb25ffb2b866f41fc620e
Chris Barden <christopher.barden@magnetimarelli.com>
2017-12-20 17:43:42 -0500

    Removed examples and restored exports I accidentally removed the wrong one a couple commits ago.  Also added a  variable to exports to test if it makes commands easier

---
+++ custom/exports.zsh
@@ -0,0 +1,5 @@
+#Custom Exports
+export APOLLO_HOME=/home/mmes/Downloads/apache-apollo-1.7.1
+export EDITOR=/usr/bin/vim
+export BROWSER=/usr/bin/chromium/chromium
+export MY_GIT="http://www.github.com/ZamieltheHunter/"
